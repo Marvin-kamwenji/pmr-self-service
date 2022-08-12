2097a66bebf566e3215c1f39b9e5b59752581827
karorimesh <karorimesh@gmail.com>
2022-08-12 13:17:33 +0300

    Routing to tenant and landlord

---
+++ src/Components/Registration/LandLordRegistration.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class LandLordRegistration extends Component {
+  render() {
+    return (
+      <div>LandLordRegistration</div>
+    )
+  }
+}
