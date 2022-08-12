d1082edd13397d7d1344c357379b9bb54e9a9397
Hanan-Hussein <hanhuss035@gmail.com>
2022-08-12 13:25:59 +0300

    st:carousel-styled carousel and some color themes

--- src/Components/Common/NavBar.jsx
+++ src/Components/Common/NavBar.jsx
@@ -4,13 +4,17 @@ import '../CSS/navbar.css';
 
 function NavBar() {
     return ( 
-        <nav className="bg-navBarBackgroundColor px-2 py-2.5">
+            <nav className="bg-navBarBackgroundColor px-2 py-2.5">
+                        <div className="container-fluid">
+
             <div className="container-fluid flex flex-wrap justify-between items-center mx-auto">
                 <a className="logoAndText flex items-center">
                     <img src={pmrlogo} className="mr-3 h-6 sm:h-9" alt="pay my rent logo" />
                     <span className=" self-center text-xl font-semibold text-navBarTextColor">Pay My Rent Self Registration</span>
                 </a>
             </div>
+            </div>
+
         </nav>
      );
 }
